eaf5cbea04696695fc7775545afcc250c29e89f6
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-01 14:31:36 +0200

    added mind-map

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('JS OK');
+/* Scaletta
+1. Recuperare gli elemti dalla pagina HTML
+2. Recuperare il valore di nome, kms ed età
+3. Calcolare prezzo base del biglietto
+4. Calcolare gli sconti
+5. Applicare gli sconti
+6. Stampare i risultati
+*/
